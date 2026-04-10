831487e517e89b5d1e8833ba3f79324c38395ed9
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-04-10 14:29:13 +0300

    numbers

---
+++ main.py
@@ -0,0 +1,22 @@
+# print("Hello", "world", sep=", ")
+# # print("first row", end='   ')
+# # print("second row")
+# input("please enter something")
+# name = input("enter your name: ")
+# print(name)
+# name = "alice"
+# other_name = name
+# print(other_name)
+# my_integer = 10
+# my_float = 0.20
+# t= 30
+# z = 2
+# d = t * z
+# print(d)
+# x = 2
+# print(x**3)
+
+
+# flat_number = 2
+# entrance_number = flat_number // 20
+# print(entrance_number)
