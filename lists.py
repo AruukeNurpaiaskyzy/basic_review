d4e972ab86ee37ec38ca24fde3031b590a722302
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2026-04-12 16:37:09 +0300

    functions

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
-fruits = ['apple', 'banana', 'cherry']
-print('banana' in fruits)
+# fruits = ['apple', 'banana', 'cherry']
+# print('banana' in fruits)
 # # print(fruits)
 
 
@@ -9,3 +9,36 @@ print('banana' in fruits)
 # my_lists = [1,2,3]
 # my_lists2 = [2,3,4]
 # print(my_lists==my_lists2)
+
+
+DEFAULT_LEVEL_EXPERIENCE = 200
+
+
+def is_leveled_up (*, current_experience: int, gained_experience: int) -> bool:
+ total_experience = current_experience+gained_experience
+ level_up = False
+ if total_experience>=DEFAULT_LEVEL_EXPERIENCE:
+    level_up = True
+    return level_up
+
+result = is_leveled_up(current_experience=150, gained_experience=60)
+print(result)  
+
+
+
+
+# DEFAULT_LEVEL_EXPERIENCE = 200
+
+# def is_leveled_up(*, current_experience: int, gained_experience: int) -> bool:
+#     total_experience = current_experience + gained_experience
+#     return total_experience >= DEFAULT_LEVEL_EXPERIENCE
+
+
+
+
+
+
+
+
+
+
